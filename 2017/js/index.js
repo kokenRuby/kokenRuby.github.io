28e522753f82aeb35efb6748cfe2b1123c47e2be
TakutoOooka <oooka.takuto@gmail.com>
2017-12-21 16:01:59 +0900

    hoge

--- 2017/js/index.js
+++ 2017/js/index.js
@@ -29,7 +29,8 @@ let App = {
             { id: 2, name: 'オブジェクト指向とは？クラスとは何か' },
             { id: 3, name: 'MVCアーキテクチャとは' },
             { id: 4, name: 'Railsコマンドを使いこなす' },
-            { id: 5, name: 'Rails開発の流れを学ぼう' }
+            { id: 5, name: 'Railsのエラー一覧' },
+            { id: 6, name: 'Rails開発の流れを学ぼう' }
           ]
         },
         {
